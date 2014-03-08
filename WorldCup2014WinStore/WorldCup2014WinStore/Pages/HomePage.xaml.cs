4b440617c7044a6d07a7ccae5848ca86a6ca9110
elecpiano <elecpiano@gmail.com>
2014-03-08 17:36:33 +0800

    switch mask

---
+++ WorldCup2014WinStore/WorldCup2014WinStore/Pages/HomePage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using WorldCup2014WinStore.Controls;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace WorldCup2014WinStore.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class HomePage : Page
+    {
+        public HomePage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void SwitchMask_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            var properties = e.GetCurrentPoint(this).Properties;
+            if (properties.IsLeftButtonPressed)
+            {
+                (sender as SwitchMask).Open();
+            }
+            else if (properties.IsRightButtonPressed)
+            {
+                (sender as SwitchMask).Close();
+            }
+        }
+    }
+}
